7f387601de2005e949bca355b42d70b1c8995030
Crowdbotics <team@crowdbotics.com>
2023-09-01 18:15:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pmsphysical-mental--43196",
+  url: "https://pmsphysical-mental--43196.botics.co",
+  api: "https://pmsphysical-mental--43196.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
